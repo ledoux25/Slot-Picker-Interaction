4c00270c048aeb8607349cd469772a442e6d4d69
ledoux25 <sanguojoseph17@gmail.com>
2025-09-20 12:17:35 -0400

    Initial Commit

---
+++ SlotPicker/SlotPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotPickerApp.swift
+//  SlotPicker
+//
+//  Created by Sanguo Joseph Ledoux on 9/20/25.
+//
+
+import SwiftUI
+
+@main
+struct SlotPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SlotPickerTests/SlotPickerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SlotPickerTests.swift
+//  SlotPickerTests
+//
+//  Created by Sanguo Joseph Ledoux on 9/20/25.
+//
+
+import Testing
+@testable import SlotPicker
+
+struct SlotPickerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
